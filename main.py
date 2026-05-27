4103cda10921dabf645d08c906de9b03bb1b4764
PythonDeveloper2026 <kamtarin.odam@gmail.com>
2026-05-27 10:22:58 +0500

    github uchun

--- main.py
+++ main.py
@@ -5,16 +5,9 @@ def kalkulyator(a, b):
 
 
 
-<<<<<<< HEAD
-print("yangi malumot")
-
-
-
-print("revert uchun")
-
-
-print("Revert uchun 2")
-=======
 def yangi():
     return "serquyosh hur olkam"
->>>>>>> parent of 8a57d83 (Yangi ozgarish)
+
+
+def yangi():
+    pass
